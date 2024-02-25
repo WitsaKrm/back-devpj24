91af10070f7282da14f9fa9b744be9f1ddd69b85
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-02-25 21:02:05 +0700

    line notify fixed

--- src/controllers/lineredirect.js
+++ src/controllers/lineredirect.js
@@ -8,21 +8,41 @@ const redirect = async (req, res) => {
     console.log("00000");
     res.sendFile(path.join(__dirname, "../../views", "line-connected.html"));
   } catch (error) {
-    const errorMessage = error.response ? error.response.data.message : error.message;
+    const errorMessage = error.response
+      ? error.response.data.message
+      : error.message;
     console.log(errorMessage);
     res.status(500).json({ redirect_error: errorMessage });
   }
 };
 
 const notify = async (req, res) => {
-  console.log('in notify');
+  console.log("in notify");
   console.log(req.body.token);
+  console.log(req.body.status);
+  const checkStt = req.body.status;
+  const status =
+    checkStt === "Success"
+      ? "Success"
+      : checkStt === "Processing"
+      ? "Processing..."
+      : checkStt === "StopButton pushed"
+      ? "StopButton pushed"
+      : "Node Error";
+
+  const message = `
+      ${req.body.username}    
+
+      อุปกรณ์  :  ${req.body.d_name} 
+      ข้อความ  :  ${req.body.message}
+      สถานะ   :  ${status}`;
+
   try {
-    if(req.body.token === undefined){
+    if (req.body.token === undefined) {
       return res.status(401).send({ message: "Notify Unsuccess." });
-    }else{
-      await line.sendLineNotify(req.body.message, req.body.token);
-    return res.status(200).send({ message: "Notify Successfully." });
+    } else {
+      await line.sendLineNotify(message, req.body.token);
+      return res.status(200).send({ message: "Notify Successfully." });
     }
   } catch (error) {
     return res.json({ error: error.response.data.message });
